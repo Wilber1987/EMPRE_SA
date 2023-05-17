36dab5cd2f3fa2713bc27f6bd6c21fae5cba8031
Wilber Jose Matus Gonzalez <wilberj1987@gmail.com>
2023-05-16 22:02:07 -0600

    FIRST COMMIT

---
+++ CAPA_DATOS/Connections/SqlADOConexion.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CAPA_DATOS
+{
+    public class SqlADOConexion
+    {
+        private static string UserSQLConexion = "";
+        public static SqlServerGDatos? SQLM;
+        public static string DataBaseName = "EMPRE_SA";
+        public static bool Anonimo = true;
+        static string SQLServer = "localhost";
+        static public bool IniciarConexionSNIBD(string user, string password)
+        {
+            try
+            {
+                Anonimo = false;
+                UserSQLConexion = "Data Source=" + SQLServer +
+                    "; Initial Catalog=" + DataBaseName + "; User ID="
+                    + user + ";Password=" + password + ";MultipleActiveResultSets=true";
+                SQLM = new SqlServerGDatos(UserSQLConexion);
+                if (SQLM.TestConnection()) return true;
+                else
+                {
+                    SQLM = null;
+                    return false;
+                }
+            }
+            catch (Exception)
+            {
+                SQLM = null;
+                Anonimo = true;
+                return false;
+                throw;
+            }
+        }
+        static public bool IniciarConexion(string user, string password)
+        {
+            try
+            {
+                Anonimo = false;
+                UserSQLConexion = "Data Source=" + SQLServer +
+                    "; Initial Catalog=" + DataBaseName + "; User ID="
+                    + user + ";Password=" + password + ";MultipleActiveResultSets=true";
+                SQLM = new SqlServerGDatos(UserSQLConexion);
+                return true;
+            }
+            catch (Exception)
+            {
+                Anonimo = true;
+                return false;
+                throw;
+            }
+        }
+        static public bool IniciarConexionAnonima()
+        {
+            try
+            {
+                Anonimo = false;
+                UserSQLConexion = "Data Source=" + SQLServer +
+                    "; Initial Catalog=" + DataBaseName +
+                    "; User ID=sa;Password=zaxscd;MultipleActiveResultSets=true";
+                SQLM = new SqlServerGDatos(UserSQLConexion);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+                throw;
+            }
+        }
+    }
+
+}
