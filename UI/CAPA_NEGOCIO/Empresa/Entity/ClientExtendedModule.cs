5da13604fec20cda0caeafc0925e4c542dcaf1a0
Wilber Jose Matus Gonzalez <wilberj1987@gmail.com>
2024-07-19 19:32:26 -0600

    profiles-ext/i-sadav#3036 clasificacion de interes y actualizacion en los clientes

--- UI/CAPA_NEGOCIO/Empresa/Entity/ClientExtendedModule.cs
+++ UI/CAPA_NEGOCIO/Empresa/Entity/ClientExtendedModule.cs
@@ -8,13 +8,17 @@ namespace DataBaseModel
 {
 	public class Catalogo_Clientes : ClientDataBaseModel.Catalogo_Clientes
 	{
-	   
-		public string? hora { get; set; }  
-		public string? Descripcion { get {
-			return $"{primer_nombre} {segundo_nombre} {primer_apellido} {segundo_apellidio}";
-		}}      
-		public int? tipoc { get; set; }      
-		public string? valor_cliente { get; set; }     
+
+		public string? hora { get; set; }
+		public string? Descripcion
+		{
+			get
+			{
+				return $"{primer_nombre} {segundo_nombre} {primer_apellido} {segundo_apellidio}";
+			}
+		}
+		public int? tipoc { get; set; }
+		public string? valor_cliente { get; set; }
 		public string? solo_acreedor { get; set; }
 		public Double? promedio { get; set; }
 		public int? id_clasificacion { get; set; }
@@ -26,9 +30,25 @@ namespace DataBaseModel
 		/*[OneToMany(TableName = "Transaction_Contratos", KeyColumn = "codigo_cliente", ForeignKeyColumn = "codigo_cliente")]*/
 		public List<Transaction_Contratos>? Transaction_Contratos { get; set; }
 
+		internal void ActualizarClasificacionInteres()
+		{
+			List<Catalogo_Clasificacion_Interes> clasificacion_Interes = new Catalogo_Clasificacion_Interes
+			{
+				orderData = [OrdeData.Desc("porcentaje")]
+			}.Get<Catalogo_Clasificacion_Interes>();
+			
+			var objetoActual = clasificacion_Interes.FirstOrDefault(o => o.id_clasificacion_interes == this.id_clasificacion_interes);
+			int indiceActual = clasificacion_Interes.IndexOf(objetoActual);
+			if (indiceActual >= 0 && indiceActual < clasificacion_Interes.Count - 1)
+			{
+				id_clasificacion_interes = clasificacion_Interes[indiceActual + 1].id_clasificacion_interes;
+				Update();
+			}
+		}
+
 		internal object? SaveClient()
 		{
-			if (new Catalogo_Clientes{identificacion = this.identificacion}.Find<Catalogo_Clientes>() != null)
+			if (new Catalogo_Clientes { identificacion = this.identificacion }.Find<Catalogo_Clientes>() != null)
 			{
 				return new ResponseService
 				{
