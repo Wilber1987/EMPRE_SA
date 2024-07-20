fc640ebc82dd7dce1e897ef2414dc5f1a49a5029
Wilber Jose Matus Gonzalez <wilberj1987@gmail.com>
2024-07-20 08:08:44 -0600

    profiles-ext/i-sadav#3036 clasificacion de interes y actualizacion en los clientes

--- UI/Program.cs
+++ UI/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc.ApplicationModels;
 
 //SqlADOConexion.IniciarConexion("sa", "zaxscd", ".", "EMPRE_SA");
 //SqlADOConexion.IniciarConexion("sa", "123", ".\\SQLEXPRESS", "EMPRE_SA");
+
 //CONEXIONES DE PRODUCCION
 SqlADOConexion.IniciarConexion("empresa", "Wmatus09%", "tcp:empresociedadanonima.database.windows.net", "EMPRE_SA");
 
